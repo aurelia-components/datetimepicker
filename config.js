1428ba4908da60d236bff91856a333f2a253b788
Tsvetina Georgieva <tsvetina.georgieva@scalefocus.com>
2016-05-05 12:34:28 +0300

    eonasdan dependency moved to npm

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ System.config({
   },
 
   map: {
-    "Eonasdan/bootstrap-datetimepicker": "github:Eonasdan/bootstrap-datetimepicker@4.17.37",
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.0",
     "npm:aurelia-polyfills@1.0.0-beta.1.1.0": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.0"
