8b9fdebbde7eb2de426f3674d6df379e362e0884
Dobromir Brezoev <dobromir.brezoev@scalefocus.com>
2016-05-12 16:29:17 +0300

    changes

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ System.config({
   map: {
     "Eonasdan/bootstrap-datetimepicker": "github:Eonasdan/bootstrap-datetimepicker@4.17.37",
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.4",
-    "jquery": "npm:jquery@2.2.3",
     "moment": "npm:moment@2.13.0",
     "npm:aurelia-polyfills@1.0.0-beta.1.1.4": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.2"
