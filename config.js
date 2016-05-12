aa6f44be678ed9f49fbd6e1c291740acb56f25ff
Dobromir Brezoev <dobromir.brezoev@scalefocus.com>
2016-05-12 10:54:23 +0300

    save

--- config.js
+++ config.js
@@ -11,47 +11,8 @@ System.config({
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.1.4",
     "jquery": "npm:jquery@2.2.3",
     "moment": "npm:moment@2.13.0",
-    "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
-    },
-    "github:jspm/nodelibs-path@0.1.0": {
-      "path-browserify": "npm:path-browserify@0.0.0"
-    },
-    "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.3"
-    },
-    "github:jspm/nodelibs-util@0.1.0": {
-      "util": "npm:util@0.10.3"
-    },
-    "npm:assert@1.3.0": {
-      "util": "npm:util@0.10.3"
-    },
     "npm:aurelia-polyfills@1.0.0-beta.1.1.4": {
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.2"
-    },
-    "npm:bootstrap@3.3.6": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:eonasdan-bootstrap-datetimepicker@4.15.35": {
-      "bootstrap": "npm:bootstrap@3.3.6",
-      "jquery": "npm:jquery@2.1.4",
-      "moment": "npm:moment@2.8.4",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:inherits@2.0.1": {
-      "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:path-browserify@0.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:process@0.11.3": {
-      "assert": "github:jspm/nodelibs-assert@0.1.0"
-    },
-    "npm:util@0.10.3": {
-      "inherits": "npm:inherits@2.0.1",
-      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
